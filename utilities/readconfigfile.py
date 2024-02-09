748d9a65d27402df962ee49d24074f40fcc9592b
priteenilkhan <priteenilkhan@gmail.com>
2024-02-09 08:29:57 +0530

    first commit

---
+++ utilities/readconfigfile.py
@@ -0,0 +1,18 @@
+import configparser
+
+config = configparser.RawConfigParser()
+config.read("C:\\Users\\Lappy\\Desktop\\pytest\\noppytest2\\Configuration\\config.ini")
+
+#config.read("..\\Configuration\\config.ini")
+
+class Readconfig:
+    @staticmethod
+    def getEmail():
+        Email = config.get('login data', 'email')
+        return Email
+
+    @staticmethod
+    def getPassword():
+        Password = config.get('login data', 'password')
+        return Password
+
